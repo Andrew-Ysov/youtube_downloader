ad7c93bf00b30d22840c62fd1867763deb1adde0
[Andrey] <[destracthioner@gmail.com]>
2024-09-06 12:00:53 +0300

    разбиение программы на модули, реализация функций отдельного скачивания видео и аудио форматов

--- main.py
+++ main.py
@@ -3,43 +3,66 @@ from pytubefix import YouTube
 link = input("введите ссылку на видео, которое хочтите скачать: ")
 yt = YouTube(link)
 available_streams = yt.streams
+download_to = 'C:/Users/destr/Downloads'
 
-video_qualities = set()
-for s in available_streams.filter(mime_type='video/mp4'):
-    if s.resolution == None:
-        continue
-    video_qualities.add(int(s.resolution[:-1]))
+def download_video(available_streams, path):
+    video_qualities = set()
+    for s in available_streams.filter(mime_type='video/mp4'):
+        if s.resolution == None:
+            continue
+        video_qualities.add(int(s.resolution[:-1]))
 
-video_qualities = sorted(video_qualities)
-for i,v in enumerate(video_qualities):
-    video_qualities[i] = str(v) + 'p'
-print(video_qualities)
-video_quality = input('Выберите качество для видео: ')
+    video_qualities = sorted(video_qualities)
+    for i,v in enumerate(video_qualities):
+        video_qualities[i] = str(v) + 'p'
+    print(video_qualities)
+    video_quality = input('Выберите качество для видео: ')
 
-if video_quality[-1] != 'p' or video_quality[-1].isdigit:
-    video_quality += 'p'
-if video_quality not in video_qualities:
-    print('Выбрано недопустимое качество видео')
+    if video_quality[-1] != 'p' or video_quality[-1].isdigit:
+        video_quality += 'p'
+    if video_quality not in video_qualities:
+        print('Выбрано недопустимое качество видео')
 
-video = available_streams.filter(resolution= video_quality, mime_type='video/mp4').first()
-print(video)
+    video = available_streams.filter(resolution= video_quality, mime_type='video/mp4').first()
+    print(video)
 
-audio_qualities = set()
-for track in available_streams.filter(mime_type= 'audio/mp4'):
-    print(track.abr)
-    audio_qualities.add(track.abr)
-audio_quality = input('Выберите качество звука: ')
+    video.download(output_path= path, filename=f'video of {video.title}.mp4', )
 
-if audio_quality[-1].isdigit():
-    audio_quality += 'kbps'
-if audio_quality not in audio_qualities:
-    print('Выбрано недопустимое качество звука')
+    video_name = f'video of {video.title}.mp4'
+    return video_name
+
+def download_audio(available_streams, path):
+    audio_qualities = set()
+    for track in available_streams.filter(mime_type= 'audio/mp4'):
+        print(track.abr)
+        audio_qualities.add(track.abr)
+    audio_quality = input('Выберите качество звука: ')
+
+    if audio_quality[-1].isdigit():
+        audio_quality += 'kbps'
+    if audio_quality not in audio_qualities:
+        print('Выбрано недопустимое качество звука')
+        
+    audio = available_streams.filter(abr=audio_quality, mime_type= 'audio/mp4').first()
+
+    audio.download(output_path= path, filename=f'audio of {audio.title} .mp4')
     
-audio = available_streams.filter(abr=audio_quality, mime_type= 'audio/mp4').first()
-print(audio)
+    audio_name = f'audio of {audio.title} .mp4'
+    return audio_name
 
-audio.download(output_path='C:/Users/destr/Downloads', filename='audio.mp4')
-video.download(output_path='C:/Users/destr/Downloads', filename='video.mp4', )
+def concatinate_video_and_audio():
+    pass
 
+type_of_content = input('Введите 1, если хотите скачать только видео, 2- только аудио, \n'
+      'что-угодно или ничего, если хотите скачать и видео и аудио: ')
+
+if type_of_content == str('1'):
+    download_video(available_streams, download_to)
+elif type_of_content == str('2'):
+    download_audio(available_streams, download_to)
+else:
+    video_name = download_video(available_streams, download_to)
+    audio_name = download_audio(available_streams, download_to)
+    concatinate_video_and_audio()
 # два файла объединяются в один полноценный файл, который сохраняется у пользователя
 # а два старых файла нужно удалить
