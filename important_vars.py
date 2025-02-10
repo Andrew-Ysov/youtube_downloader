c0b5c4919705ca89e574cdd07f704b6bee5720b8
Усов Андрей <destracthioner@gmail.com>
2025-02-10 10:46:33 +0300

    Update important_vars.py

--- important_vars.py
+++ important_vars.py
@@ -1,2 +1,8 @@
-# Эта переменная обозначает путь до места, куда будут скачиваться видео
-download_to = 'C:/Users/destr/Downloads'
+''' Переменная обозначает путь до места, куда будут скачиваться видео '''
+
+
+def main():
+  download_to = 'C:/Users/destr/Downloads'
+
+if __name__ == '__main__':
+  main()
